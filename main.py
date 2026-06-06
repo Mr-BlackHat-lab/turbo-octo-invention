6e49ce0499ff490dfbd2dc6455e5d7c5eda0954e
Mr-BlackHat-lab <prahalad.hat@outlook.com>
2026-06-06 22:32:29 +0530

    added database orm connection

--- main.py
+++ main.py
@@ -1,10 +1,47 @@
-﻿from datetime import datetime
+﻿from contextlib import asynccontextmanager
+from datetime import datetime
 from random import randint
-from typing import Any
+from typing import Annotated, Any
 
-from fastapi import FastAPI, HTTPException, Response
+from fastapi import Depends, FastAPI, HTTPException, Response
+from sqlmodel import Field, Session, SQLModel, create_engine, select
 
-app = FastAPI(root_path="/api/v1")
+
+class Campaign(SQLModel, table=True):
+    campaign_id: int | None = Field(default=None, primary_key=True)
+    name: str = Field(index=True)
+    due_date: datetime | None = Field(default=None, index=True)
+    created_at: str | None = Field(default=datetime.now(), index=True)
+
+
+sqlite_file_name = "database.db"
+sqlite_url = f"sqlite:///{sqlite_file_name}"
+
+connect_args = {"check_same_thread": False}
+engine = create_engine(sqlite_url, connect_args=connect_args)
+
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+
+SessionDep = Annotated[Session, Depends(get_session)]
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    create_db_and_tables()
+    # with Session(engine) as session:
+    # if not session.exec(select())
+    yield
+
+
+app = FastAPI(root_path="/api/v1", lifespan=lifespan)
 
 
 @app.get("/")
