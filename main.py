f3465f6e0386854050b991dcf0a48fdcf1974886
Mr-BlackHat-lab <prahalad.hat@outlook.com>
2026-06-10 20:40:48 +0530

    added get method in for sqllite

--- main.py
+++ main.py
@@ -92,52 +92,56 @@ data: Any = [
 ]
 
 
+# @app.get("/campaigns")
+# async def read_campaigns():
+#     return {"campaigns": data}
 @app.get("/campaigns")
-async def read_campaigns():
-    return {"campaigns": data}
+async def read_campaigns(session: SessionDep):
+    data = session.exec(select(Campaign)).all()
+    return{"campaigns": data}
 
 
-@app.get("/campaigns/{id}")
-async def read_campaign_id(id: int):
-    for campaign in data:
-        if campaign.get("campaign_id") == id:
-            return {"campaigns": campaign}
-    raise HTTPException(status_code=404)
+# @app.get("/campaigns/{id}")
+# async def read_campaign_id(id: int):
+#     for campaign in data:
+#         if campaign.get("campaign_id") == id:
+#             return {"campaigns": campaign}
+#     raise HTTPException(status_code=404)
 
 
-@app.post("/campaigns", status_code=201)
-async def create_campaign(body: dict[str, Any]):
+# @app.post("/campaigns", status_code=201)
+# async def create_campaign(body: dict[str, Any]):
 
-    new: Any = {
-        "campaign_id": randint(100, 1000),
-        "name": body.get("name"),
-        "due_date": body.get("due_date"),
-        "created_at": datetime.now(),
-    }
+#     new: Any = {
+#         "campaign_id": randint(100, 1000),
+#         "name": body.get("name"),
+#         "due_date": body.get("due_date"),
+#         "created_at": datetime.now(),
+#     }
 
-    data.append(new)
-    return {"campaign": new}
-
-
-@app.put("/campaigns/{id}")
-async def update_campaign(id: int, body: dict[str, Any]):
-    for index, campaign in enumerate(data):
-        if campaign.get("campaign_id") == id:
-            updated: Any = {
-                "campaign_id": id,
-                "name": body.get("name"),
-                "due_date": body.get("due_date"),
-                "created_at": campaign.get("created_at"),
-            }
-            data[index] = updated
-            return {"campaign": updated}
-    raise HTTPException(status_code=404)
+#     data.append(new)
+#     return {"campaign": new}
 
 
-@app.delete("/campaigns/{id}")
-async def delet_campaign(id: int):
-    for index, campaign in enumerate(data):
-        if campaign.get("campaign_id") == id:
-            data.pop(index)
-            return Response(status_code=204)
-    raise HTTPException(status_code=404)
+# @app.put("/campaigns/{id}")
+# async def update_campaign(id: int, body: dict[str, Any]):
+#     for index, campaign in enumerate(data):
+#         if campaign.get("campaign_id") == id:
+#             updated: Any = {
+#                 "campaign_id": id,
+#                 "name": body.get("name"),
+#                 "due_date": body.get("due_date"),
+#                 "created_at": campaign.get("created_at"),
+#             }
+#             data[index] = updated
+#             return {"campaign": updated}
+#     raise HTTPException(status_code=404)
+
+
+# @app.delete("/campaigns/{id}")
+# async def delet_campaign(id: int):
+#     for index, campaign in enumerate(data):
+#         if campaign.get("campaign_id") == id:
+#             data.pop(index)
+#             return Response(status_code=204)
+#     raise HTTPException(status_code=404)
